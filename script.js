51fa6faa29bde239b3f0a4531e700333656038fd
Shivam Maurya <shivammaurya2903@gmail.com>
2025-09-14 23:30:05 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const filterBtns = document.querySelectorAll('.filter-btn');
+const posts = document.querySelectorAll('.post');
+
+filterBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // Toggle active class
+    filterBtns.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    const category = btn.dataset.cat;
+    posts.forEach(post => {
+      const postCats = post.dataset.cat.split(' ');
+      if (category === 'all' || postCats.includes(category)) {
+        post.style.display = 'block';
+      } else {
+        post.style.display = 'none';
+      }
+    });
+  });
+});
+
+
+const scrollLine = document.querySelector('.scroll-line');
+    window.addEventListener('scroll', () => {
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrollPercent = (scrollTop / docHeight) * 100;
+      scrollLine.style.width = `${scrollPercent}%`;
+    });
+
+
